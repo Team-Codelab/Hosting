3726a19a3107d14949069a15c9475dfc8f8e209a
mrhid6 <dominic.richardson1@hotmail.co.uk>
2016-07-28 18:32:44 +0100

    web hosting page added

---
+++ Beta/web-hosting.php
@@ -0,0 +1,53 @@
+<html>
+<?php include_once("includes/template/head.php"); ?>
+<body>
+
+<div class="main-page-wrapper">
+    <?php include_once("includes/template/top-background.php");?>
+    <section id="content">
+        <div class="white-section">
+            <div class="container">
+                <div class="row">
+                    <div class="span12">
+                        <h1 class="center-heading">Powerful Web Hosting</h1>
+                        <h4 class="center-heading thin-heading">Web Hosting Made Easy And Affordable!</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <?php include_once("includes/hosting_prices.php");?>
+                </div>
+                <div class="row">
+                    <div class="span4 hosting-extras">
+                        <img src="style/images/icons/32x32/bandwidth.png"/>
+                        <h5>99.9% Uptime</h5>
+                        <p>
+                            At HostXtra we can guarantee 99.9% uptime, so that your business can keep running.
+                        </p>
+                    </div>
+                    <div class="span4 hosting-extras">
+                        <img src="style/images/icons/32x32/dedicated-server.png"/>
+                        <h5>SSD Disk Storage</h5>
+                        <p>
+                            At HostXtra we only use SSD disk storage for super-fast and reliable website hosting.
+                        </p>
+                    </div>
+                    <div class="span4 hosting-extras">
+                        <img src="style/images/icons/32x32/secure-server.png"/>
+                        <h5>Fully Supported SSL</h5>
+                        <p>
+                            SSL Certificates are fully supported with HostXtra website hosting, so have peace and mind your website is secure.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include_once("includes/template/footer.php");?>
+</div>
+
+<a href="#" class="go-top" style="display: block;">Top</a>
+
+<script src="js/main.js"></script>
+</body>
+
+</html>
